1f76b8ca0d9fe85e5da9fb7ddb8727c54b1134b0
Pietro Henrique <pietrohenriquealvesdossantospi@gmail.com>
2025-08-22 21:49:09 -0300

    script: create script logic

---
+++ chat-gpt/javascript/condicionais.js
@@ -0,0 +1,12 @@
+//exercicio - 1
+let idadeTest = prompt("Qual é a sua idade?");
+
+if (idadeTest >= 18) {
+
+    alert("Você é maior de idade.");
+
+ } else {
+
+    alert("Você é menor de idade.");
+    
+}
---
+++ chat-gpt/javascript/script.js
@@ -0,0 +1,22 @@
+console.log("Hello, World!");
+//variavel sao pequenas caixa onde eu coloco valor 
+
+//exemplo de variavel
+// let nome = "Pietro";
+// let test = prompt("Qual é o seu nome?");
+// let idade = prompt("Qual é a sua idade?");
+// var cidade = "São Paulo";
+// const pi = 3.14;
+
+// alert("Meu nome é " + test + "Eu tenho " + idade + " anos.");
+
+let idadeTest = prompt("Qual é a sua idade?");
+
+if (idadeTest >= 18) {
+
+    alert("Você é maior de idade.");
+
+}else {
+
+    alert("Você é menor de idade.");
+}
